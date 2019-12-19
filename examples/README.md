165579735e0bfb0211cec6d2dd8e43f6b5228bb1
Mike A <32306794+DismissedGuy@users.noreply.github.com>
2019-12-19 16:20:45 +0100

    Initialize repository

---
+++ examples/README.md
@@ -0,0 +1,2 @@
+# Examples
+* **Basic Example**:       [basic.py](basic.py)
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="brainfuck-interpreter-DismissedGuy",
+    version="1.0.0",
+    author="Mike A.",
+    author_email="dismissed.is.a.guy@gmail.com",
+    description="An object-oriented highly flexible brainfuck interpreter.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DismissedGuy/brainfuck-interpreter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Interpreters"
+    ],
+    python_requires='>=3.6',
+)
